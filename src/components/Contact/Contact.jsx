76d0935863e492a79c7b95e9fa6f3fed8e87dea0
Madhaneeswaran <madhaneeswaran@juhomi.com>
2022-02-13 19:36:08 +0530

    Edit contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -15,7 +15,7 @@ const Contact = () => {
         <Fade bottom duration={1000} delay={800} distance="30px">
           <div className="contact-wrapper">
             <p className="contact-wrapper__text">
-              {cta || 'Would you like to contact with me? Drop a mail down below'}
+              {cta || 'Would you like to contactme? Drop a mail'}
             </p>
             <a
               target="_blank"
